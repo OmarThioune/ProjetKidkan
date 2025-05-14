f9c5164ade5938fbaf8f1bffed4f728efea23e85
HassanRammal <samram005@outlook.com>
2025-05-14 11:42:50 -0400

    modif de controleur register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/recherche';
+    protected $redirectTo = '/compte';
 
     /**
      * Create a new controller instance.
